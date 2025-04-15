df2abab9f304ae3efab6ff6e666e9f0e38ec9969
Roman <roman.gorshkov19@gmail.com>
2025-04-15 15:04:09 +0300

    added-preloader-and-private-routes

--- src/layouts/PrivateLayout/PrivateLayout.tsx
+++ src/layouts/PrivateLayout/PrivateLayout.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
 import Navbar from '../../components/shared/Navbar/Navbar';
 
 import styles from './PrivateLayout.module.scss';
+import { useAppSelector } from '../../store/storeHooks';
 
 const PrivateLayout: React.FC = () => {
+  const { user } = useAppSelector((state) => state.auth);
+
   return (
-    <div className={styles.privateLayout}>
-      <Navbar />
-      <div className={styles.privateLayout__mainContent}>
-        <Outlet />
-      </div>
+    <div>
+      {user ? (
+        <div className={styles.privateLayout}>
+          <Navbar />
+          <div className={styles.privateLayout__mainContent}>
+            <Outlet />
+          </div>
+        </div>
+      ) : (
+        <Navigate to="login" replace />
+      )}
     </div>
   );
 };
